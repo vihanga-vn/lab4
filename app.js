732fdc253de6f548d64d601f7edd3b636c13c2ae
Vihanga Nipun <vihanganipun31@gmail.com>
2026-02-23 22:07:32 +0530

    Add feature: Read File

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-const myModule = require('./my-module.js'); 
-console.log(myModule.myFunction()); 
+// Define the condition first
+const condition = true; 
+
+const myPromise = new Promise((resolve, reject) => { 
+  if (condition) { 
+    resolve('Success!'); 
+  } else { 
+    reject('Failure!'); 
+  } 
+}); 
+ 
+myPromise.then((result) => { 
+  console.log(result); 
+}).catch((error) => { 
+  console.log(error); 
+});
--- my-module.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  myFunction: () => "Hello from the custom module!"
-};
